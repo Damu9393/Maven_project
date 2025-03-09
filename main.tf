7b2f982e4e0dd661d9f3ca3451725b5eef2f70d3
Damu9393 <damodaratadimarri1992@gmail.com>
2025-03-09 23:34:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
-sudo yum install maven -y
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-00db8dadb36c9815e"
+    instance_type = "t2.nano"
+    tags = {
+      Name = "dev12345678"
+    }
+}
